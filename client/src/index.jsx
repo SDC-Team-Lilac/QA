085c13b499125b8bff01292cc23aa3232ffeaecf
Justin Cheng <jucheng.ca@gmail.com>
2023-04-12 13:48:09 -0700

    first commit

---
+++ client/src/index.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import { createRoot } from 'react-dom/client';
+
+const App = () => {
+  return (
+    <div>
+      Hello World!
+    </div>
+  )
+}
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+root.render(<App />);
